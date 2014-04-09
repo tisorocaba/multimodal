aa5cd8c13b899642af82cd80a8f935da2d876c7f
Thiago Ribeiro <thiagofribeiro@gmail.com>
2014-04-08 22:03:33 -0300

    Changin global to local Marionette

--- src/multimodal.js
+++ src/multimodal.js
@@ -1,6 +1,6 @@
 var _ = require('underscore'),
-	Backbone = require('backbone');
-Marionette = require('marionette');
+	Backbone = require('backbone'),
+    Marionette = require('marionette');
 
 jQuery = $ = require('jquery');
 
